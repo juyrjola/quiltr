5e027f631e9d916f1f18bf67f8a9a2e72d699939
Juha Yrjölä <juha.yrjola@iki.fi>
2019-01-15 16:16:57 +0200

    One more fix

--- R/io.R
+++ R/io.R
@@ -13,11 +13,20 @@
 #' qload("examples/wine", "quality/red")
 #' qload("akarve/seattle_911", "responses")
 qload <- function(pkg, file) {
+    #info_df <- qparse(pkg, file)
+    #if(paste(info_df$class) != "TableNode") {
+    #    stop("Not a TableNode")
+    #}
+
     pkg_pythonic <- stringr::str_replace_all(pkg, "/", "\\.")
     pkg_name <- paste0("quilt.data.", pkg_pythonic)
-    data <- reticulate::import(module = pkg_name)
+    data <- reticulate::import(module = pkg_name, convert = FALSE)
     file <- stringr::str_replace_all(file, "/", "$")
 
     df <- eval(parse(text = paste0("data$", file, "()")))
-    df
+
+    tmp <- tempfile(pattern = "quilt", fileext = "feather")
+    pyfeather <- reticulate::import(module = "feather")
+    pyfeather$write_dataframe(df$copy(), tmp)
+    feather::read_feather(tmp)
 }
